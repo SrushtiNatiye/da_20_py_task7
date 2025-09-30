e1278377e9c1334ff18b0523e1d48fa8d7e42a94
SrushtiNatiye <srushtinatiye@gmail.com>
2025-10-01 01:56:39 +0530

    Add files via upload

---
+++ arc_data.py
@@ -0,0 +1,123 @@
+companyName = "ARC"
+
+contactNumber = 1234567890
+
+classTime = 7.30
+
+isClass = True
+
+facultyName = ["Sagar", "Vipul", "Rohit", "Rakesh"],
+
+subjectNames = ('sub1', 'sub2', 'sub3', 'sub4')
+
+studentData =[
+    {
+    "name": "stud1",
+    "age": 26,
+    "isMale": True,
+    "DA": True,
+    "marks": [90, 95, 88, 92],
+    "address": {
+        "city": "Pune",
+        "state": "Maharashtra",
+        "pincode": 411057
+    }
+},
+    {
+    "name": "stud2",
+    "age": 28,
+    "isMale": True,
+    "DA": False,
+    "marks": [85, 80, 78, 82],
+    "address": {
+        "city": "Mumbai",
+        "state": "Maharashtra",
+        "pincode": 400001
+    }
+},
+    {
+    "name": "stud3",
+    "age": 22,
+    "isMale": False,
+    "DA": True,
+    "marks": [88, 90, 92, 94],
+    "address": {
+        "city": "Delhi",
+        "state": "Delhi",
+        "pincode": 110001
+    }
+},
+    {
+    "name": "stud4",
+    "age": 25,
+    "isMale": True,
+    "DA": False,
+    "marks": [70, 75, 78, 72],
+    "address": {
+        "city": "Bangalore",
+        "state": "Karnataka",
+        "pincode": 560001
+    }
+},
+    {
+    "name": "stud5",
+    "age": 23,
+    "isMale": False,
+    "DA": True,
+    "marks": [92, 88, 90, 95],
+    "address": {
+        "city": "Chennai",
+        "state": "Tamil Nadu",
+        "pincode": 600001
+    }
+},
+    {
+    "name": "stud6",
+    "age": 24,
+    "isMale": True,
+    "DA": False,
+    "marks": [78, 82, 80, 75],
+    "address": {
+        "city": "Hyderabad",
+        "state": "Telangana",
+        "pincode": 500001
+    }
+},
+    {
+    "name": "stud7",
+    "age": 22,
+    "isMale": False,
+    "DA": True,
+    "marks": [85, 88, 90, 82],
+    "address": {
+        "city": "Kolkata",
+        "state": "West Bengal",
+        "pincode": 700001
+    }
+},
+    {
+    "name": "stud8",
+    "age": 26,
+    "isMale": True,
+    "DA": True,
+    "marks": [90, 92, 88, 95],
+    "address": {
+        "city": "Ahmedabad",
+        "state": "Gujarat",
+        "pincode": 380001
+    }
+},
+    {
+    "name": "stud9",
+    "age": 21,
+    "isMale": False,
+    "DA": False,
+    "marks": [75, 70, 72, 78],
+    "address": {
+        "city": "Jaipur",
+        "state": "Rajasthan",
+        "pincode": 302001
+    }
+}
+
+]
